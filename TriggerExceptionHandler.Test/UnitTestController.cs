d4df29e6f3316d31e6d4c43f036e3fd0e2d52d10
Fabio <f.fabrizi91@gmail.com>
2019-09-30 12:53:53 +0200

    refactoring

--- TriggerExceptionHandler.Test/UnitTestController.cs
+++ TriggerExceptionHandler.Test/UnitTestController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -14,54 +15,49 @@ namespace TriggerExceptionHandler.Test
         [Fact]
         public async Task TestExceptionHandler()
         {
-            using (var fixture = new TestServerFixture())
-            {
-                var response = await fixture.Client.GetAsync("/api/demo/");
-                Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+            using var fixture = new TestServerFixture();
+            var response = await fixture.Client.GetAsync("/api/demo/");
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
 
-                var content = await response.Content.ReadAsStringAsync();
-                Assert.NotEmpty(content);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
 
-                var problemDetails = JsonConvert.DeserializeObject<ValidationProblemDetails>(content);
+            var problemDetails = JsonConvert.DeserializeObject<ValidationProblemDetails>(content);
 
-                Assert.NotEmpty(problemDetails.Title);
-                //Assert.NotEmpty(problemDetails.Detail);
-                Assert.NotEmpty(problemDetails.Type);
-                Assert.True(problemDetails.Status.HasValue);
-                Assert.True(problemDetails.Status.Value == (int)response.StatusCode);
-            }
+            Assert.NotEmpty(problemDetails.Title);
+            Assert.NotEmpty(problemDetails.Type);
+            Assert.True(problemDetails.Status.HasValue);
+            Assert.True(problemDetails.Status.Value == (int)response.StatusCode);
         }
-
 
         [Fact]
         public async Task TestModelStateHandler()
         {
-            using (var fixture = new TestServerFixture())
+            using var fixture = new TestServerFixture();
+            var parameters = new StringContent(JsonConvert.SerializeObject(new
             {
-                var parameters = new StringContent(JsonConvert.SerializeObject(new
-                {
-                    WrongParameter = "test"
-                }),
-                    Encoding.UTF8,
-                    "application/json"
-                );
+                WrongParameter = "test"
+            }),
+                Encoding.UTF8,
+                "application/json"
+            );
 
-                var response = await fixture.Client.PostAsync("/api/demo/validate/", parameters);
-                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            var response = await fixture.Client.PostAsync("/api/demo/validate/", parameters);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
 
-                var content = await response.Content.ReadAsStringAsync();
-                Assert.NotEmpty(content);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(content);
 
-                var problemDetails = JsonConvert.DeserializeObject<ValidationProblemDetails>(content);
+            var problemDetails = JsonConvert.DeserializeObject<ValidationProblemDetails>(content);
 
-                Assert.NotEmpty(problemDetails.Title);
-                Assert.NotEmpty(problemDetails.Detail);
-                Assert.NotEmpty(problemDetails.Type);
-                Assert.True(problemDetails.Status.HasValue);
-                Assert.True(problemDetails.Status.Value == (int)response.StatusCode);
-                Assert.NotNull(problemDetails.Errors);
-                Assert.NotEmpty(problemDetails.Errors);
-            }
+            Assert.NotEmpty(problemDetails.Title);
+            Assert.NotEmpty(problemDetails.Detail);
+            Assert.NotEmpty(problemDetails.Type);
+            Assert.True(problemDetails.Status.HasValue);
+            Assert.True(problemDetails.Status.Value == (int)response.StatusCode);
+            Assert.NotNull(problemDetails.Errors);
+            Assert.NotEmpty(problemDetails.Errors);
         }
     }
 }
