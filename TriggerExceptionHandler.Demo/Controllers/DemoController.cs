9916588be61a77558748f93c0788908fbf704f1a
Fabio <f.fabrizi91@gmail.com>
2019-09-30 12:50:42 +0200

    custom exception

--- TriggerExceptionHandler.Demo/Controllers/DemoController.cs
+++ TriggerExceptionHandler.Demo/Controllers/DemoController.cs
@@ -2,31 +2,31 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using TriggerExceptionHandler.Attributes;
+using TriggerExceptionHandler.Demo.Exceptions;
 using TriggerExceptionHandler.Models;
 
 namespace TriggerExceptionHandler.Demo.Controllers
 {
     [Route("api/[controller]")]
-	[ApiController]
-	public class DemoController : Controller
-	{
-		public IActionResult Index()
-		{
-			throw new Exception("Expected exception");
-		}
-
+    [ApiController]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new ExpectedException("Expected exception");
+        }
 
         [HttpPost("validate")]
-		[ValidateModelState]
-		public IActionResult ValidateModelState([FromBody]DtoParameters p)
-		{
-			return Ok(p);
-		}
+        [ValidateModelState]
+        public IActionResult ValidateModelState([FromBody]DtoParameters p)
+        {
+            return Ok(p);
+        }
 
-		public class DtoParameters
-		{
-			[Required]
-			public string Name { get; set; }
-		}
-	}
+        public class DtoParameters
+        {
+            [Required]
+            public string Name { get; set; }
+        }
+    }
 }
---
+++ TriggerExceptionHandler.Demo/Exceptions/ExpectedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TriggerExceptionHandler.Demo.Exceptions
+{
+    [Serializable]
+    internal class ExpectedException : Exception
+    {
+        public ExpectedException()
+        {
+        }
+
+        public ExpectedException(string message) : base(message)
+        {
+        }
+
+        public ExpectedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExpectedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
