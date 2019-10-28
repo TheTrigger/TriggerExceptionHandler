b6f7a03ec77c25257eadec00a19d9fc02e4cc2e6
Fabio <f.fabrizi91@gmail.com>
2019-10-28 10:37:02 +0100

    fixed http status code

--- TriggerExceptionHandler/Extensions/ExceptionHandlerExtensions.cs
+++ TriggerExceptionHandler/Extensions/ExceptionHandlerExtensions.cs
@@ -15,7 +15,7 @@ namespace TriggerExceptionHandler.Extensions
         public static IApplicationBuilder UseTriggerExceptionHandler(this IApplicationBuilder app, string applicationName, ILogger logger = null, Ext2HttpCode exceptionsCode = null)
         {
             if (applicationName == null) throw new ArgumentNullException(nameof(applicationName));
-            
+
             if (exceptionsCode == null)
                 exceptionsCode = new Ext2HttpCode();
 
@@ -23,23 +23,26 @@ namespace TriggerExceptionHandler.Extensions
             {
                 var showDetails = Debugger.IsAttached;
 
+                // ExceptionHandlerFeature
                 var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
                 var exception = errorFeature.Error;
 
-                var eventId = new EventId(context.TraceIdentifier.GetHashCode(), nameof(Exception));
-
-                logger?.LogError(eventId: eventId, exception: exception, message: exception.Message, context.Request);
+                var exceptionName = exception.GetType().Name;
+                var eventId = new EventId(context.TraceIdentifier.GetHashCode(), exceptionName);
 
                 var problemDetails = new ProblemDetails
                 {
                     Detail = showDetails ? exception.ToString() : null,
                     Instance = $"urn:{applicationName}:{eventId.Id.ToString()}",
                     Title = exception.Message,
-                    Type = exception.GetType().Name,
+                    Type = exceptionName,
                     Status = exceptionsCode[exception]
                 };
 
-                await context.Response.WriteJsonAsync(problemDetails);
+                logger?.LogError(eventId: eventId, exception: exception, message: exception.Message, context.Request);
+
+                context.Response.StatusCode = problemDetails.Status.Value;
+                await context.Response.WriteJsonAsync(problemDetails).ConfigureAwait(false);
             }));
 
             return app;
